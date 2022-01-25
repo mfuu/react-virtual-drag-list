8327037851b334c10006489999dad570b2c58c2c
mf-note <446310957@qq.com>
2022-01-25 10:49:34 +0800

    init
    init package and rollup

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+/*!
+ * react-virtual-drag-list v0.0.1
+ * open source under the MIT license
+ * https://github.com/mf-note/react-virtual-drag-list#readme
+ */
+
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+})((function () { 'use strict';
+
+
+
+}));
---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import babel from 'rollup-plugin-babel'
+const packageJson = require('./package.json')
+const version = packageJson.version
+const homepage = packageJson.homepage
+
+const banner = `
+/*!
+ * react-virtual-drag-list v${version}
+ * open source under the MIT license
+ * ${homepage}
+ */
+`
+
+export default {
+  external: ['vue'],
+  input: 'src/index.js',
+  output: {
+    format: 'umd',
+    file: 'dist/index.js',
+    sourcemap: false,
+    globals: {
+      react: 'React'
+    },
+    banner: banner.replace(/\n/, '')
+  },
+  plugins: [
+    babel()
+  ]
+}
