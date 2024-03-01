a70f86e7a3334f001e6c46aed9f120b9a9436a15
YOKE <mfuu.me@gmail.com>
2024-03-01 11:49:14 +0800

    added props: `scroller`, `debounceTime`, `throttleTime`

--- src/props.ts
+++ src/props.ts
@@ -6,28 +6,56 @@ type RenderFunc<T> = (
   props: { style?: React.CSSProperties }
 ) => React.ReactNode;
 
+export interface Group {
+  name: string;
+  put: boolean | Array<string>;
+  pull: boolean | 'clone';
+  revertDrag: boolean;
+}
+
+export interface SortState {
+  item: any;
+  key: any;
+  index: number;
+}
+
+export interface FromTo {
+  index: number;
+  list: any[];
+}
+
+export interface DropState {
+  changed: boolean;
+  list: any[];
+  item: any;
+  key: any;
+  from: FromTo;
+  to: FromTo;
+}
+
 export interface VirtualProps<T> {
-  dataSource: T[];
   dataKey: string;
-  direction?: 'vertical' | 'horizontal';
+  dataSource: T[];
+
   keeps?: number;
   size?: number;
-
-  pageMode?: boolean;
-  disabled?: boolean;
-  draggable?: Function | string;
+  group?: Group | string;
   handle?: Function | string;
-  group?: object | string;
+  scroller?: HTMLElement | Window | Document;
+  direction?: 'vertical' | 'horizontal';
+  debounceTime?: number;
+  throttleTime?: number;
 
   delay?: number;
+  disabled?: boolean;
+  draggable?: string;
   keepOffset?: boolean;
   autoScroll?: boolean;
-  scrollThreshold?: number;
-  pressDelay?: number;
-  pressDelayOnTouchOnly?: boolean;
   fallbackOnBody?: boolean;
+  scrollThreshold?: number;
+  delayOnTouchOnly?: boolean;
 
-  style?: object;
+  style?: CSSStyleDeclaration;
   className?: string;
 
   rootTag?: string;
@@ -36,14 +64,16 @@ export interface VirtualProps<T> {
   headerTag?: string;
   footerTag?: string;
 
-  itemStyle?: object;
+  itemStyle?: CSSStyleDeclaration;
   itemClass?: string;
-  rootStyle?: object;
+  rootStyle?: CSSStyleDeclaration;
   rootClass?: string;
-  wrapStyle?: object;
+  wrapStyle?: CSSStyleDeclaration;
   wrapClass?: string;
+  headerClass?: string;
+  headerStyle?: CSSStyleDeclaration;
 
-  ghostStyle?: object;
+  ghostStyle?: CSSStyleDeclaration;
   ghostClass?: string;
   chosenClass?: string;
   animation?: number;
@@ -52,27 +82,38 @@ export interface VirtualProps<T> {
   header?: React.ReactNode;
   footer?: React.ReactNode;
 
-  'v-top'?: Function;
-  'v-bottom'?: Function;
-  'v-drag'?: Function;
-  'v-drop'?: Function;
-  'v-add'?: Function;
-  'v-remove'?: Function;
+  'v-top'?: () => void;
+  'v-bottom'?: () => void;
+  'v-drag'?: (params: SortState) => void;
+  'v-add'?: (params: SortState) => void;
+  'v-remove'?: (params: SortState) => void;
+  'v-drop'?: (params: DropState) => void;
+}
+
+export interface VirtualComponentRef {
+  getSize: (key: any) => number;
+  getOffset: () => number;
+  getClientSize: () => number;
+  getScrollSize: () => number;
+  scrollToTop: () => void;
+  scrollToIndex: (index: number) => void;
+  scrollToOffset: (offset: number) => void;
+  scrollToBottom: () => void;
 }
 
 export interface BaseProps {
   Tag?: string;
-  style?: object;
+  style?: CSSStyleDeclaration;
   className?: string;
   sizeKey?: string;
-  onSizeChange?: Function;
+  onSizeChange?: (key, size: number) => void;
   children?: React.ReactNode | ((ref: React.RefObject<any>) => React.ReactElement);
 }
 
 export interface ObserverProps {
   dataKey: string | number;
   sizeKey?: string;
-  onSizeChange?: Function;
+  onSizeChange?: (key, size: number) => void;
   children?: React.ReactNode | ((ref: React.RefObject<any>) => React.ReactElement);
 }
 
